cfe19ab37bcfffab2439e1e9a649a9b07080d3ee
Ryo Hashiguchi <ryo12redstone@gmail.com>
2016-11-23 22:58:24 +0900

    Ignore repeated track

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ var itunes = require('playback')
 var FormData = require('form-data')
 
 function call_api(path, method, opts) {
-  var params = opts.params || {};
-  var headers = opts.headers || {};
+  var params = opts.params || {}
+  var headers = opts.headers || {}
   var data = new FormData()
   Object.keys(params).forEach(key => {
     data.append(key, params[key])
@@ -38,23 +38,34 @@ call_api('/api/users/' + USER_ID, 'put', {params: {secret: USER_SECRET}})
   })
 
 function start_app(token) {
+  var last_song = {}
   itunes.on('playing', data => {
-    var name = data.name, album = data.album, artist = data.artist;
-    call_api('/api/users/' + USER_ID + '/play', 'post', {
-      headers: {
-        'Authorization': 'Bearer ' + token
-      },
-      params: {
-        artist: artist,
-        album: album,
-        song: name
-      }
-    })
-      .then(resp => {
-        console.log('Play: ' + artist + ', ' + album + ', ' + name)
-      })
-      .catch(resp => {
-        console.error(resp)
-      })
+    var same = data.name === last_song.name
+      && data.artist === last_song.artist
+      && data.album === last_song.album
+    last_song = data
+    if (!same) {
+      send(token, data)
+    }
   })
+}
+
+function send(token, data) {
+  var name = data.name, artist = data.artist, album = data.album
+  call_api('/api/users/' + USER_ID + '/play', 'post', {
+    headers: {
+      'Authorization': 'Bearer ' + token
+    },
+    params: {
+      artist: artist,
+      album: album,
+      song: name
+    }
+  })
+    .then(resp => {
+      console.log('Play: ' + artist + ', ' + album + ', ' + name)
+    })
+    .catch(resp => {
+      console.error(resp)
+    })
 }
